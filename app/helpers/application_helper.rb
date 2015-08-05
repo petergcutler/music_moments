bc64bb6b73bffceb5033743cbb62e856388e8cdd
Paul Doulatshahi <paul.doulatshahi@gmail.com>
2015-08-05 13:24:40 -0400

    a lot of css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
     youtube_id = youtube_url.split("=").last
     content_tag(:iframe, nil, src: "//www.youtube.com/embed/#{youtube_id}")
   end
+
+  def signed_in
+    !session[:user].nil?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments
     end
+    root to: 'posts#index'
   get '/sign_in', to: 'users#sign_in'
   post '/sign_in', to: 'users#sign_in!'
   get '/sign_up', to: 'users#sign_up'
   post '/sign_up', to: 'users#sign_up!'
-  get '/sign_out', to: 'users#sign_out'
+  get '/sign_out', to: 'users#sign_out!'
 end
